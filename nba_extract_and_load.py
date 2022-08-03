eedd380a61ca7a6c3e37869ff7f48e31b492311c
Diana Patron <patrondiana1309@gmail.com>
2022-08-02 19:19:31 -0500

    Update config imports

--- nba_extract_and_load.py
+++ nba_extract_and_load.py
@@ -1,20 +1,21 @@
 import json
 import requests
 import pandas as pd
+import config as conf
 from sqlalchemy import create_engine
-from config import *
 
-engine = create_engine(f"mysql://{db_username}:{db_pwd}@{db_host}:{db_port}/{db_name}")
+
+engine = create_engine(f"mysql://{conf.db_username}:{conf.db_pwd}@{conf.db_host}:{conf.db_port}/{conf.db_name}")
 con = engine.connect()
 
 headers = {
-	"X-RapidAPI-Key": api_key,
-	"X-RapidAPI-Host": api_host
+	"X-RapidAPI-Key": conf.api_key,
+	"X-RapidAPI-Host": conf.api_host
 }
 querystring = {"page": "0", "per_page": "100"}
 
 # Get teams
-url_teams = f"{api_base_url}/teams"
+url_teams = f"{conf.api_base_url}/teams"
 teams = requests.request("GET", url_teams, headers=headers)
 teams_json = json.loads(teams.text)
 df_teams = pd.DataFrame(teams_json["data"])
